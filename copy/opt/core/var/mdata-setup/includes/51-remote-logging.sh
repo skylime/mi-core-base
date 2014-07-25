d34ae282b288d531d24e71fd5496a5b183295e8f
Thomas Merkel <tm@core.io>
2014-07-25 15:28:17 +0200

    Add remote logging server variables

---
+++ copy/opt/core/var/mdata-setup/includes/51-remote-logging.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Create remote logging file if mdata is configured
+
+RSYSLOG_CONF_D='/opt/local/etc/rsyslog.d/'
+
+echo '# remote-logging' > ${RSYSLOG_CONF_D}/remote-logging.conf
+if mdata-get syslog_ssl 1>/dev/null 2>&1 && \
+   mdata-get syslog_host 1>/dev/null 2>&1; then
+	# Allow only ssl for remote logging
+	mdata-get syslog_ssl > ${RSYSLOG_CONF_D}/remote-logging-ca.pem
+	echo '# use tls'                            >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo '$DefaultNetstreamDriver gtls'         >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo '$ActionSendStreamDriverMode 1'        >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo '$ActionSendStreamDriverAuthMode anon' >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo "\$DefaultNetstreamDriverCAFile ${RSYSLOG_CONF_D}/remote-logging-ca.pem" \
+		>> ${RSYSLOG_CONF_D}/remote-logging.conf
+	# Setup remote logging host
+	echo '# send (all) messages'        >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo "*.* @@$(mdata-get syslog_host)" >> ${RSYSLOG_CONF_D}/remote-logging.conf
+fi
+
+# Restart rsyslog after config change
+svcadm restart svc:/pkgsrc/rsyslog:default
