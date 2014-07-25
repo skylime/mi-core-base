7a081face1e6838e80a4d37cd169103901c8b249
Thomas Merkel <tm@core.io>
2014-07-25 15:14:41 +0200

    Add munin-node as default

---
+++ copy/opt/core/var/mdata-setup/includes/50-munin-node.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Configure munin-node allowed connections
+
+MUNIN_CONF='/opt/local/etc/munin/munin-node.conf'
+
+cp ${MUNIN_CONF}.tpl ${MUNIN_CONF}
+if mdata-get munin_master_allow 1>/dev/null 2>&1; then
+	echo "# mdata-get munin_master_allow" >> ${MUNIN_CONF}
+	for allow in $(mdata-get munin_master_allow); do
+		echo "cidr_allow ${allow}" >> ${MUNIN_CONF}
+	done
+fi
