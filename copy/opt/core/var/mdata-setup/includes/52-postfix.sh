dd4e11eee51f0f1b5998b218d2064cffaded8ea9
Thomas Merkel <tm@core.io>
2014-08-18 16:27:20 +0200

    allow also mail send without authentication

--- copy/opt/core/var/mdata-setup/includes/52-postfix.sh
+++ copy/opt/core/var/mdata-setup/includes/52-postfix.sh
@@ -11,9 +11,14 @@ if mdata-get mail_smarthost 1>/dev/null 2>&1; then
 	# postfix authentication
 	if mdata-get mail_auth_user 1>/dev/null 2>&1 && \
 	   mdata-get mail_auth_pass 1>/dev/null 2>&1; then
+		sed -i "s:smtp_sasl_auth_enable.*:smtp_sasl_auth_enable = yes:g" \
+			${POSTFIX_CONF}main.cf
 		echo "${SMART_HOST} $(mdata-get mail_auth_user):$(mdata-get mail_auth_pass)" \
 			> ${POSTFIX_CONF}relay_passwords
 		postmap hash:${POSTFIX_CONF}relay_passwords
+	else
+		sed -i "s:smtp_sasl_auth_enable.*:smtp_sasl_auth_enable = no:g" \
+			${POSTFIX_CONF}main.cf
 	fi
 
 	# postalias for root emails
