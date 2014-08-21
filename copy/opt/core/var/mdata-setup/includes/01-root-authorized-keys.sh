80018fdf371b2e78ce3ef8c849c80b944046f847
Thomas Merkel <tm@core.io>
2014-08-21 14:31:26 +0200

    be sure you create an ssh dir

--- copy/opt/core/var/mdata-setup/includes/01-root-authorized-keys.sh
+++ copy/opt/core/var/mdata-setup/includes/01-root-authorized-keys.sh
@@ -2,8 +2,10 @@
 # Configure root ssh authorized_keys file if available via mdata
 
 if mdata-get root_authorized_keys 1>/dev/null 2>&1; then
+	mkdir -p /root/.ssh
 	echo "# This file is managed by mdata-get root_authorized_keys" \
 		> /root/.ssh/authorized_keys
 	mdata-get root_authorized_keys >> /root/.ssh/authorized_keys
+	chmod 700 /root/.ssh
 	chmod 644 /root/.ssh/authorized_keys
 fi
