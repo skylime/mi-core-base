a2d9289d621158b127564372bf175d778997c11d
Thomas Merkel <tm@core.io>
2014-10-02 12:25:30 +0200

    new: dev: add leading number to the rsyslog remote log config file

--- copy/opt/core/var/mdata-setup/includes/51-remote-logging.sh
+++ copy/opt/core/var/mdata-setup/includes/51-remote-logging.sh
@@ -3,18 +3,18 @@
 
 RSYSLOG_CONF_D='/opt/local/etc/rsyslog.d/'
 
-echo '# remote-logging' > ${RSYSLOG_CONF_D}/remote-logging.conf
+echo '# remote-logging' > ${RSYSLOG_CONF_D}/10-remote-logging.conf
 if mdata-get syslog_host 1>/dev/null 2>&1; then
 	# Allow only ssl for remote logging
-	echo '# use tls'                            >> ${RSYSLOG_CONF_D}/remote-logging.conf
-	echo '$DefaultNetstreamDriver gtls'         >> ${RSYSLOG_CONF_D}/remote-logging.conf
-	echo '$ActionSendStreamDriverMode 1'        >> ${RSYSLOG_CONF_D}/remote-logging.conf
-	echo '$ActionSendStreamDriverAuthMode anon' >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo '# use tls'                            >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
+	echo '$DefaultNetstreamDriver gtls'         >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
+	echo '$ActionSendStreamDriverMode 1'        >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
+	echo '$ActionSendStreamDriverAuthMode anon' >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	echo "\$DefaultNetstreamDriverCAFile /etc/ssl/certs/ca-certificates.crt" \
-		>> ${RSYSLOG_CONF_D}/remote-logging.conf
+		>> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	# Setup remote logging host
-	echo '# send (all) messages'        >> ${RSYSLOG_CONF_D}/remote-logging.conf
-	echo "*.* @@$(mdata-get syslog_host)" >> ${RSYSLOG_CONF_D}/remote-logging.conf
+	echo '# send (all) messages'        >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
+	echo "*.* @@$(mdata-get syslog_host)" >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 fi
 
 # Restart rsyslog after config change
