2c93ae28e5a4e707c8202df18a91a983979f3a8d
Thomas Merkel <tm@core.io>
2015-03-27 13:55:05 +0100

    Enable nullmailer by default if smarthost exists

--- copy/var/zoneinit/includes/17-nullmailer.sh
+++ copy/var/zoneinit/includes/17-nullmailer.sh
@@ -1,4 +1,3 @@
 if mdata-get mail_smarthost 1>/dev/null 2>&1; then
-	echo XXX add smf manifest
-	#/usr/sbin/svcadm enable svc:/pkgsrc/nullmailer:default
+	/usr/sbin/svcadm enable svc:/pkgsrc/nullmailer:default
 fi
