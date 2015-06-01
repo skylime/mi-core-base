7837e0723e0cec08a97833d2922433dc0de5029f
Thomas Merkel <tm@core.io>
2015-06-01 16:58:51 +0200

    Switch to own rsyslog config for the future.

--- copy/opt/core/var/mdata-setup/includes/51-remote-logging.sh
+++ copy/opt/core/var/mdata-setup/includes/51-remote-logging.sh
@@ -5,8 +5,9 @@ RSYSLOG_CONF_D='/opt/local/etc/rsyslog.d/'
 
 echo '# remote-logging' > ${RSYSLOG_CONF_D}/10-remote-logging.conf
 if mdata-get syslog_host 1>/dev/null 2>&1; then
+	echo '$ModLoad imtcp'                       >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	# Allow only ssl for remote logging
-	echo '# use tls'                            >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
+	echo '# use tls'                            >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	echo '$DefaultNetstreamDriver gtls'         >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	echo '$ActionSendStreamDriverMode 1'        >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
 	echo '$ActionSendStreamDriverAuthMode anon' >> ${RSYSLOG_CONF_D}/10-remote-logging.conf
