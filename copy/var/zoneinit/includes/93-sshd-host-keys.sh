1553ad1301011b44f6b6133c7d1a379aa1659bad
Thomas Merkel <tm@core.io>
2015-06-01 18:12:05 +0200

    workaround for ssh host key mdata saving

--- copy/var/zoneinit/includes/93-sshd-host-keys.sh
+++ copy/var/zoneinit/includes/93-sshd-host-keys.sh
@@ -3,6 +3,10 @@
 SSH_HOSTS='/var/ssh'
 
 for key in dsa_key dsa_key_pub rsa_key rsa_key_pub; do
+	if [[ ! -f ${SSH_HOSTS}/ssh_host_${key} ]]; then
+		# Ugly workaround if files not exists (because no SSH or SSH not started)
+		continue
+	fi
 	current_key=${current_key:-$(mdata-get ssh_host_${key} 2>/dev/null)} || \
 		current_key=$(cat ${SSH_HOSTS}/ssh_host_${key});
 	mdata-put ssh_host_${key} "${current_key}"
