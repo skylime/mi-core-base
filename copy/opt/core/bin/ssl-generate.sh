47feccd5065a078f34f8c6f0cfed3bdaa0508ed3
Thomas Merkel <tm@core.io>
2017-01-20 14:00:35 +0100

    rename prefix option to filename

--- copy/opt/core/bin/ssl-generate.sh
+++ copy/opt/core/bin/ssl-generate.sh
@@ -5,22 +5,22 @@
 
 # Defaults
 CN=$(hostname)
-PREFIX='server'
+FILENAME='server'
 
 # Help function
 function help() {
-	echo "${0} -d <DESTINATION> [-c common name] [-p prefix]"
+	echo "${0} -d <DESTINATION> [-c common name] [-f filename]"
 	exit 1
 }
 
 # Option parameters
 if (( ${#} < 1 )); then help; fi
 
-while getopts ":d:c:p:" opt; do
+while getopts ":d:c:f:" opt; do
 	case "${opt}" in
 		d) DESTINATION=${OPTARG} ;;
 		c) CN=${OPTARG} ;;
-		p) PREFIX=${OPTARG} ;;
+		p) FILENAME=${OPTARG} ;;
 		*) help ;;
 	esac
 done
@@ -32,13 +32,13 @@ if [[ ! -d "$DESTINATION" ]]; then
 fi
 
 # Generate key and csr via OpenSSL
-openssl req -newkey rsa:2048 -keyout ${DESTINATION}/${prefix}.key \
-            -out ${DESTINATION}/${prefix}.csr -nodes \
+openssl req -newkey rsa:2048 -keyout ${DESTINATION}/${FILENAME}.key \
+            -out ${DESTINATION}/${FILENAME}.csr -nodes \
             -subj "/C=DE/L=Raindbow City/O=Aperture Science/OU=Please use valid ssl certificate/CN=${CN}"
 
 # Generate self signed ssl certificate from csr via OpenSSL
-openssl x509 -in ${DESTINATION}/${prefix}.csr -out ${DESTINATION}/${prefix}.crt -req \
-             -signkey ${DESTINATION}/${prefix}.key -days 128
+openssl x509 -in ${DESTINATION}/${FILENAME}.csr -out ${DESTINATION}/${FILENAME}.crt -req \
+             -signkey ${DESTINATION}/${FILENAME}.key -days 128
 
 # Create one PEM file which contains certificate and key
-cat ${DESTINATION}/${prefix}.crt ${DESTINATION}/${prefix}.key > ${DESTINATION}/${prefix}.pem
+cat ${DESTINATION}/${FILENAME}.crt ${DESTINATION}/${FILENAME}.key > ${DESTINATION}/${FILENAME}.pem
