9e5861d86f015affec0a67451ac68ab0069edcac
Thomas Merkel <tm@core.io>
2017-01-23 14:44:29 +0100

    Use extra script for let's encrypt
    We will not run let's encrypt by default. The configuration for let's encrypt should
    be done in the image which need let's encrypt or is based on it.

--- copy/opt/core/bin/ssl-letsencrypt.sh
+++ copy/opt/core/bin/ssl-letsencrypt.sh
@@ -1,3 +1,49 @@
 #!/usr/bin/env bash
+# This script will generate a let's encrypt ssl certificate if possible.
+# It's a simple wrapper script for the official certbot client, because
+# we maybe need the webserver feature to receive a validation.
 
+# Defaults
+CN=$(hostname)
+EMAIL=$(mdata-get mail_adminaddr 2>/dev/null)
 
+# Help function
+function help() {
+	echo "${0} [-c common name] [-m mail address]"
+	exit 1
+}
+
+# Option parameters
+while getopts "c:m:" opt; do
+	case "${opt}" in
+		c) CN=${OPTARG} ;;
+		m) EMAIL=${OPTARG} ;;
+		*) help ;;
+	esac
+done
+
+if host ${CN} 1>/dev/null 2>&1; then
+	# Setup account email address to mail_adminaddr if exists
+	if [[ -z ${EMAIL} ]]; then
+		EMAIL='--register-unsafely-without-email'
+	else
+		EMAIL="--email ${EMAIL}"
+	fi
+
+	# Run initial certbot command to create account and certificate
+	certbot certonly
+		--standalone \
+		--agree-tos \
+		--quiet \
+		--text \
+		--non-interactive
+		${EMAIL} \
+		--domains ${HOST}
+
+	# Create cronjob to automatically check or renew the certificate two
+	# times a day
+	CRON='0 0,12 * * * certbot renew --text --non-interactive --quiet'
+	(crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
+else
+	echo 'The common name you provided could not be resolved by DNS!' 1>&2;
+fi
