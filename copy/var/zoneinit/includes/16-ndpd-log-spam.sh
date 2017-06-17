5f55face80baec5454760bd65c97924096191286
Thomas Merkel <tm@core.io>
2017-06-17 18:44:49 +0200

    Provide empty file if addrconf is not set

--- copy/var/zoneinit/includes/16-ndpd-log-spam.sh
+++ copy/var/zoneinit/includes/16-ndpd-log-spam.sh
@@ -2,4 +2,6 @@
 if mdata-get sdc:nics | grep -qv addrconf 1>/dev/null 2>&1; then
 	echo '# https://github.com/joyent/smartos-live/issues/501' > /etc/inet/ndpd.conf
 	echo 'ifdefault StatelessAddrConf off'                    >> /etc/inet/ndpd.conf
+else
+	echo > /etc/inet/ndpd.conf
 fi
