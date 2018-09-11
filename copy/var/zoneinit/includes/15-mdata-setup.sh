882239eabe8d31733c14881d9725b1be8689388b
Thomas Merkel <tm@core.io>
2018-09-11 13:57:21 +0200

    Remove mdata-setup support and use only existing zoneinit service

--- copy/var/zoneinit/includes/15-mdata-setup.sh
+++
@@ -1,2 +0,0 @@
-# Run mdata setup on provision state
-/opt/core/bin/mdata-setup
--- copy/var/zoneinit/includes/50-munin-node.sh
+++ copy/var/zoneinit/includes/50-munin-node.sh
@@ -1,2 +1,15 @@
+#!/usr/bin/env bash
+# Configure munin-node allowed connections
+
+MUNIN_CONF='/opt/local/etc/munin/munin-node.conf'
+
+cp ${MUNIN_CONF}.tpl ${MUNIN_CONF}
+if mdata-get munin_master_allow 1>/dev/null 2>&1; then
+	echo "# mdata-get munin_master_allow" >> ${MUNIN_CONF}
+	for allow in $(mdata-get munin_master_allow); do
+		echo "allow ^${allow//\./\\.}$" >> ${MUNIN_CONF}
+	done
+fi
+
 # Enable munin service
 /usr/sbin/svcadm enable svc:/pkgsrc/munin-node:default
