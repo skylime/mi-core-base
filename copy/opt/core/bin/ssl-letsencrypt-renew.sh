e1387f7e6ec93a35d82b5bd3d06bc7abc9124552
Thomas Merkel <tm@core.io>
2020-08-31 16:17:51 +0200

    Support --debug for ssl-letsencrypt-renew.sh to receive some additional information

--- copy/opt/core/bin/ssl-letsencrypt-renew.sh
+++ copy/opt/core/bin/ssl-letsencrypt-renew.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # Helper script to renew all SSL Let's Encrypt SSL certificates easily
 
+[ "${1^}" = "--debug" ] || quiet='--quiet'
+
 /opt/local/bin/certbot renew \
 	--text \
 	--non-interactive \
-	--quiet \
+	${quiet} \
 	--pre-hook "/opt/local/etc/letsencrypt/pre-hook.sh" \
 	--post-hook "/opt/local/etc/letsencrypt/post-hook.sh" \
 	--renew-hook "/opt/local/etc/letsencrypt/renew-hook.sh"
