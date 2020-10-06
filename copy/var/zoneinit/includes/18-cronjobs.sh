3db02a0b2e226fb30803d3b14b4d6c71c8b86978
Thomas Merkel <tm@core.io>
2020-10-06 13:08:43 +0200

    Report znapzend errors after 2 errors a day

--- copy/var/zoneinit/includes/18-cronjobs.sh
+++ copy/var/zoneinit/includes/18-cronjobs.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # create cronjob for ssl-expire script
 CRON='0 9 * * 1 /opt/core/bin/ssl-expire.sh
-15 * * * * /opt/core/bin/check-log /var/adm/messages "(znapzend.*ERROR)"
+15 * * * * /opt/core/bin/check-log /var/adm/messages "(znapzend.*ERROR)" 2
 '
 (crontab -l 2>/dev/null || true; echo "$CRON" ) | sort | uniq | crontab
