a07e570f32cd8901dbf4fcc900792c10b11dee6b
Thomas Merkel <tm@core.io>
2021-05-05 10:14:16 +0200

    file not required in zoneinit because already in postinit

--- copy/var/zoneinit/includes/99-admin-user-init.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-# Run admin user init.sh from home folder if exists. The script should run as
-# user who owns it.
-
-if [ -x /home/admin/init.sh ]; then
-	( sudo -u admin bash -x /home/admin/init.sh 2>&1 ) |\
-		mailx -s "postinit /home/admin/init.sh" root
-fi
