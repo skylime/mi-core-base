c1c4ac439c9da112660b7b116f3c0e4281fbaee6
Thomas Merkel <tm@core.io>
2021-05-06 18:37:21 +0200

    Reset postinit for re-provision

--- copy/var/zoneinit/includes/10-core-smf.sh
+++ copy/var/zoneinit/includes/10-core-smf.sh
@@ -6,3 +6,9 @@ CORE_SVC_DIR='/opt/core/lib/svc/manifest/'
 for xml in ${CORE_SVC_DIR}*; do
 	svccfg import ${xml}
 done
+
+# Postinit is required to run also on re-provision and images which
+# are derived from the core-base image. For that reason it's required
+# to set postinit to false.
+svccfg -s postinit 'setprop application/done = false'
+svcadm refresh postinit
