7708a8029a860afb2c84ae90fb04dc23a06bd5d9
Thomas Merkel <tm@core.io>
2022-09-29 21:39:07 +0200

    Enable delegate dataset (ZFS) for /home
    Enable managed ZFS (delegate dataset) for useradd and user homes. Modify
    existing users (admin) to use /home/admin dataset.

--- copy/var/zoneinit/includes/04-delegated-dataset.sh
+++ copy/var/zoneinit/includes/04-delegated-dataset.sh
@@ -15,18 +15,25 @@ function install_skel() {
 }
 
 if zfs list ${DDS} 1>/dev/null 2>&1; then
-	if ! zfs list ${DDS}/home/admin 1>/dev/null 2>&1; then
-		# Create delegate dataset for home/admin
+	echo "MANAGE_ZFS=YES" >> /etc/default/useradd
+
+	if ! zfs list -H -o mountpoint ${DDS}/home/admin 2>/dev/null | grep "/home/admin"; then
+		if [ -d "/home/admin" ]; then
+			rm -r /home/admin
+		fi
+
 		zfs create -p ${DDS}/home/admin
-
-		# Remove the folder as well because of delegate dataset
-		rm -r /home/admin
-
-		# Set mount point and fix permissions
-		zfs set mountpoint=/home/admin ${DDS}/home/admin
-
-		install_skel
+	else
+		zfs set mountpoint=none ${DDS}/home/admin
 	fi
+
+	if ! zfs get mountpoint -o source ${DDS}/home 2>/dev/null | grep "local"; then
+		zfs set mountpoint=/home ${DDS}/home
+	fi
+
+	zfs set mountpoint=/home/admin ${DDS}/home/admin
+	install_skel
 else
+	mkdir -p /home/admin
 	install_skel
 fi
